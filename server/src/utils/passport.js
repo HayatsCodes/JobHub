ebeafe49c28b229dca3ccc99d4400b357473018e
Hayats <hayatudeenabdulrahman@gmail.com>
2023-04-17 12:58:56 +0100

    configured passportjs

---
+++ server/src/utils/passport.js
@@ -0,0 +1,40 @@
+const LocalStrategy = require('passport-local').Strategy;
+
+async function verifyUser()
+// creating and exporting passport configuration function
+module.exports = (passport, User) => {
+    passport.use(
+        new LocalStrategy(
+            {
+                usernameField: 'email',
+                passwordField: 'password',
+            },
+            (email, password, done) => {
+                try {
+                    User.findOne({ email }, async (err, user) => {
+                        if (err) { return done(err); }
+                        if (!user) {
+                            return done(null, false, { message: 'Incorrect email or password' });
+                        }
+                        const isPasswordMatch = await bcrypt.compare(password, user.password);
+                        if (!isPasswordMatch) {
+                            return done(null, false, { message: 'Incorrect email or password' });
+                        }
+                        return done(null, user);
+                    });
+                } catch (err) {
+                    return done(err);
+                }
+                
+            }
+        )
+    );
+
+    passport.serializeUser((user, done) => {
+        done(null, { id: user._id, role: user.role });
+    });
+
+    passport.deserializeUser((user, done) => {
+        return done(null, user);
+    });
+}
