edd9842bfada3f7288e019167125879bf74a7e85
Hayats <hayatudeenabdulrahman@gmail.com>
2023-04-20 10:23:21 +0100

    add jobSchema

---
+++ server/src/models/job.model.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const jobSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    location: {
+        type: String,
+        required: true,
+    },
+    salary: {
+        type: Number,
+        required: true,
+    },
+    employer: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    status: {
+        type: String,
+        enum: ['draft', 'published'],
+        default: 'draft',
+    },
+
+}, { timestamps: true }
+);
