05ba91052322c73156af4df0ac791c55c54b1364
Hayats <hayatudeenabdulrahman@gmail.com>
2023-04-25 11:02:20 +0100

    test Should get all published jobs with admin and user role

--- server/src/routes/jobs/jobs.controller.js
+++ server/src/routes/jobs/jobs.controller.js
@@ -1,6 +1,6 @@
- const jobModel = require('../../models/job.model')
+const jobModel = require('../../models/job.model')
 
- async function addJob(req, res) {
+async function addJob(req, res) {
     try {
         const job = new jobModel({
             ...req.body,
@@ -8,90 +8,97 @@
         });
         await job.save();
         res.status(201).json(job);
-    } catch(err) {
+    } catch (err) {
         console.log(err.stack)
-        return res.status(400).json({error: 'Invalid job'});
+        return res.status(400).json({ error: 'Invalid job' });
     }
 }
 
 async function getJobs(req, res) {
     try {
-        const jobs = await jobModel.find({status: 'published'});
+        const jobs = await jobModel.find({ status: 'published' });
         res.status(200).json(jobs);
     } catch (err) {
         console.log(err.stack)
-        return res.status(400).json({error: 'Encountered an error'});
+        return res.status(400).json({ error: 'Encountered an error' });
     }
 }
 
 async function getJob(req, res) {
     try {
-        const { id }= req.params;
+        const { id } = req.params;
         const job = await jobModel.findById(id);
         if (job && job.status === 'published') {
             return res.status(200).json(job);
         }
-        res.status(404).json({error: 'Job not found'});
+        res.status(404).json({ error: 'Job not found' });
     } catch (err) {
         console.log(err.stack)
-        return res.status(404).json({error: 'Job not found'});
+        return res.status(404).json({ error: 'Job not found' });
     }
 }
 
 async function getEmployerJobs(req, res) {
     try {
-        if (req.user.role === 'admin' || req.user.role === 'user') {
-            const employerId = req.query.employerId
+        if (req.user.role === 'admin') {
+            const employerId = req.query.employerId;
             if (!employerId) {
-                return res.status(400).json( {err: 'Invalid request'} )
+                return res.status(400).json({ err: 'Invalid request' });
             }
-            const employerJobs = await jobModel.find({createdBy: employerId})
+            const employerJobs = await jobModel.find({ createdBy: employerId });
             return res.status(200).json(employerJobs);
 
         } else if (req.user.role === 'employer') {
             const employerId = req.user.id;
-            const employerJobs = await jobModel.find({createdBy: employerId})
+            const employerJobs = await jobModel.find({ createdBy: employerId });
             return res.status(200).json(employerJobs);
         }
-       
+
     } catch (err) {
-        return res.status(400).json({error: 'Invalid request'});
+        return res.status(400).json({ error: 'Invalid request' });
     }
 }
 
 async function getEmployerJob(req, res) {
     try {
-            const jobId = req.params.id;
-            const employerJob = await jobModel.findById(jobId);
-            if (!employerJob) {
-                return res.status(400).json( {err: 'Job not found'} );
-            }
-            return res.status(200).json(employerJob);
+        const jobId = req.params.id;
+        const employerJob = await jobModel.findById(jobId);
+        if (!employerJob) {
+            return res.status(404).json({ error: 'Job not found' });
+        }
+        const employerId = employerJob.createdBy
+        if (req.user.id !== employerId.toString()) {
+            return res.status(401).json({ error: 'Unauthorized' });
+        }
+        return res.status(200).json(employerJob);
+
     } catch (err) {
-        return res.status(400).json({error: 'Invalid request'});
+        console.log(err.stack);
+        return res.status(400).json({ error: 'Invalid request' });
     }
 }
+
 
 async function updateJob(req, res) {
     try {
         if (req.user.role === 'admin') {
             const jobId = req.params.id;
-            const employerJob = await jobModel.findByIdAndUpdate(jobId, req.body, {new: true});
+            const employerJob = await jobModel.findByIdAndUpdate(jobId, req.body, { new: true });
             return res.status(200).json(employerJob);
         } else if (req.user.role === 'employer') {
-            const  jobId = req.params.id;
-            let employerJob = await jobModel.find({createdBy: req.user.id, _id: jobId});
+            const jobId = req.params.id;
+            let employerJob = await jobModel.find({ createdBy: req.user.id, _id: jobId });
             if (employerJob.length < 1) {
-                return res.status(400).json({error: 'Job not found!'});
+                return res.status(400).json({ error: 'Job not found!' });
             }
-            employerJob = await jobModel.findByIdAndUpdate(jobId, req.body, {new: true});
+            employerJob = await jobModel.findByIdAndUpdate(jobId, req.body, { new: true });
 
             return res.status(200).json(employerJob);
         }
 
     } catch (err) {
         console.log(err.stack);
-        return res.status(400).json({error: 'Invalid request'});
+        return res.status(400).json({ error: 'Invalid request' });
     }
 }
 
@@ -105,10 +112,10 @@ async function deleteJob(req, res) {
             }
             res.status(200).json({ message: 'Job deleted successfully' });
         } else if (req.user.role === 'employer') {
-            const  jobId = req.params.id;
-            let employerJob = await jobModel.find({createdBy: req.user.id, _id: jobId});
+            const jobId = req.params.id;
+            let employerJob = await jobModel.find({ createdBy: req.user.id, _id: jobId });
             if (employerJob.length < 1) {
-                return res.status(400).json({error: 'Job not found!'});
+                return res.status(400).json({ error: 'Job not found!' });
             }
             employerJob = await jobModel.findByIdAndDelete(jobId);
             if (!employerJob) {
@@ -119,7 +126,7 @@ async function deleteJob(req, res) {
 
     } catch (err) {
         console.log(err.stack);
-        return res.status(400).json({error: 'Invalid request'});
+        return res.status(400).json({ error: 'Invalid request' });
     }
 }
 
--- server/src/routes/jobs/jobs.route.test.js
+++ server/src/routes/jobs/jobs.route.test.js
@@ -3,7 +3,6 @@ const { MongoMemoryServer } = require('mongodb-memory-server');
 const request = require('supertest');
 require('dotenv').config();
 const app = require('../../app');
-const userModel = require('../../models/user.model');
 const jobModel = require('../../models/job.model');
 
 
@@ -14,12 +13,14 @@ describe('jobRoute', () => {
     // let agent;
     let admin;
     let employer;
+    let employer2;
     let user;
     let adminAgent;
     let employerId;
     let employerAgent;
     let userAgent;
     let jobId;
+    let jobId2;
 
     beforeAll(async () => {
         mongo = await MongoMemoryServer.create();
@@ -43,6 +44,14 @@ describe('jobRoute', () => {
             firstName: 'One',
             lastName: 'Employer',
             email: 'employer1@example.com',
+            password: 'password',
+            role: 'employer',
+        }
+
+        employer2 = {
+            firstName: 'Two',
+            lastName: 'Employer',
+            email: 'employer2@example.com',
             password: 'password',
             role: 'employer',
         }
@@ -127,9 +136,42 @@ describe('jobRoute', () => {
             expect(job.salary).toBe(jobData.salary);
             expect(job.status).toBe('published');
 
+            // Employer 2
+            const newAgent = await request.agent(app);
+            await newAgent
+                .post('/api/auth/signup')
+                .send(employer2)
+                .expect(201);
+
+
+            const jobData2 = {
+                title: 'Data Scientist',
+                description: 'We are seeking for a data scientist',
+                location: 'San Francisco',
+                salary: 90000,
+                status: 'published'
+            }
+
+            await newAgent
+                .post('/api/jobs')
+                .send(jobData2)
+                .expect('Content-Type', /json/)
+                .expect(201);
+
+
+            const job2 = await jobModel.findOne({ title: jobData2.title });
+            expect(job2).toBeDefined();
+            expect(job2.description).toBe(jobData2.description);
+            expect(job2.location).toBe(jobData2.location);
+            expect(job2.salary).toBe(jobData2.salary);
+            expect(job2.status).toBe('published');
+
+
+
             employerAgent = agent;
             employerId = job.createdBy;
             jobId = job._id;
+            jobId2 = job2._id;
         });
 
         test('Should not create a job with a user role', async () => {
@@ -186,15 +228,11 @@ describe('jobRoute', () => {
             expect(res.body[0].status).toBe('published');
         });
 
-        test('Should be able to get employer jobs by employerId with user role', async () => {
+        test('Should not be able to get employer jobs by employerId with user role', async () => {
             const res = await userAgent
                 .get(`/api/jobs/employer?employerId=${employerId}`)
                 .expect('Content-Type', /json/)
-                .expect(200)
-
-            expect(res.body[0]).toBeDefined();
-            expect(res.body[0].title).toBe('Data Analyst');
-            expect(res.body[0].status).toBe('published');
+                .expect(401)
         });
 
     });
@@ -207,8 +245,83 @@ describe('jobRoute', () => {
             .expect('Content-Type', /json/)
             .expect(200)
 
+
         expect(res.body.title).toBe('Data Analyst');
         expect(res.body.status).toBe('published');
+        });
+
+        test('Should not get another employer job with employer role', async () => {
+            const res = await employerAgent
+                .get(`/api/jobs/employer/${jobId2}`)
+                .expect('Content-Type', /json/)
+                .expect(401)
+
+            expect(res.body.error).toBe('Unauthorized');
         })
+
+    });
+
+    describe('GET /api/jobs', () => {
+        // @todo:
+        // test: get all jobs with admin role
+        //  test: get all jobs with user role
+        //  test: should not get all jobs with employer role
+        
+        test('Should get all published jobs with admin role', async () => {
+            const res = await adminAgent
+            .get('/api/jobs')
+            .expect('Content-Type', /json/)
+            .expect(200)
+
+
+            expect(res.body[0]).toBeDefined();
+            expect(res.body[0].title).toBe('Data Analyst');
+            expect(res.body[0].status).toBe('published');
+
+            expect(res.body[1]).toBeDefined();
+            expect(res.body[1].title).toBe('Data Scientist');
+            expect(res.body[1].status).toBe('published');
+        });
+
+        test('Should get all published jobs with user role', async () => {
+            const res = await userAgent
+            .get('/api/jobs')
+            .expect('Content-Type', /json/)
+            .expect(200)
+
+
+            expect(res.body[0]).toBeDefined();
+            expect(res.body[0].title).toBe('Data Analyst');
+            expect(res.body[0].status).toBe('published');
+
+            expect(res.body[1]).toBeDefined();
+            expect(res.body[1].title).toBe('Data Scientist');
+            expect(res.body[1].status).toBe('published');
+        });
+
+
+        
+    })
+
+    describe('GET /api/jobs/:id', () => {
+        // @todo:
+        // test: get a joby by id with admin role
+        // test: get a job by id with user role
+    })
+
+    describe('PATCH /api/jobs/:id', () => {
+        // @todo:
+        // test: update a joby by id with admin role
+        // test: update employer owned job by id with employer role
+        // test: should not update a job by id with user role
+        // test: should not update another employer job with employer role
+    })
+
+    describe('DELETE /api/jobs/:id', () => {
+        // @todo:
+        // test: delete a joby by id with admin role
+        // test: delete employer owned job by id with employer role
+        // test: should not delete a job by id with user role
+        // test: should not delete another employer job with employer role
     })
 });
