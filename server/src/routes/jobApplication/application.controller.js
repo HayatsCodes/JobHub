89584d3ab15190d656c39775e7ee2c6312f25d90
Hayats <hayatudeenabdulrahman@gmail.com>
2023-05-01 20:59:41 +0100

    handled error

--- server/src/routes/jobApplication/application.controller.js
+++ server/src/routes/jobApplication/application.controller.js
@@ -3,29 +3,35 @@ const applicationModel = require('../../models/jobApplication.model');
 const jobModel = require('../../models/job.model')
 
 async function addApplication(req, res) {
-    const application = new applicationModel({
-        ...req.body,
-        user: req.user.id,
-    });
-
-    const job = await jobModel.findById(req.body.jobId);
-    application.employer = job.createdBy;
-
-    if (req.file) {
-        application.resume = {
-            name: req.file.filename,
-            data: fs.readFileSync(req.file.path),
-            contentType: 'application/pdf'
-        }
+    try {
+        const application = new applicationModel({
+            ...req.body,
+            user: req.user.id,
+        });
+    
         const job = await jobModel.findById(req.body.jobId);
         application.employer = job.createdBy;
-        await application.save();
-        //   delete the uploaded file from the local disc
-        fs.unlinkSync(req.file.path);
-
-        return res.status(201).json({ ...application.toObject(), resume: req.file.filename });
+    
+        if (req.file) {
+            application.resume = {
+                name: req.file.filename,
+                data: fs.readFileSync(req.file.path),
+                contentType: 'application/pdf'
+            }
+            const job = await jobModel.findById(req.body.jobId);
+            application.employer = job.createdBy;
+            await application.save();
+            //   delete the uploaded file from the local disc
+            fs.unlinkSync(req.file.path);
+    
+            return res.status(201).json({ ...application.toObject(), resume: req.file.filename });
+        }
+        return res.status(400).json({ error: 'Can not create application' });
+    } catch(error)  {
+        console.error(error);
+        return res.status(500).json({ error: 'Internal server error' });
     }
-    return res.status(400).json({ error: 'Can not create application' });
+    
 }
 
 
