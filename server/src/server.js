5cacb0a694de3d23ddff6a52962b8da7c659a1fa
Hayats <hayatudeenabdulrahman@gmail.com>
2023-05-25 17:36:14 +0100

    added server port

--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,7 @@ const http = require('http');
 require('dotenv').config();
 const app = require('./app');
 const mongoConnect = require('./config/mongo');
+const PORT = process.env.PORT || 8000
 
 const server = http.createServer(app);
 
